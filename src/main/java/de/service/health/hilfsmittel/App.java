a454b5953d8aa3c26aff55ec42c3ff3c9fdd7f65
Manuel Blechschmidt <manuel.blechschmidt@service-health.de>
2025-04-01 09:51:30 +0200

    Added initial data for Hilfsmittel AI Search

---
+++ src/main/java/de/service/health/hilfsmittel/App.java
@@ -0,0 +1,10 @@
+package de.service.health.hilfsmittel;
+
+import javax.xml.bind.JAXBContext;
+
+public class App {
+	JAXBContext jaxbContext = JAXBContext.newInstance(WurzelCtp.class);
+	public static void main(String[] args) {
+		
+	}
+}
