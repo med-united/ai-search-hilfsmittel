fd2f6fdc9dec200f6b1c8e0638c6bb15fae009e5
Alex Kontsur <alexander.kontsur@gmail.com>
2025-04-28 12:04:20 +0400

    EPA-466 * deployment

--- linux-service/update-from-git.sh
+++ linux-service/update-from-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo git pull
-sudo mvn clean package -Dquarkus.package.type=fast-jar -DskipTests
+git pull
+mvn clean package -Dquarkus.package.type=fast-jar -DskipTests
 sudo systemctl stop aisearch
 sudo rm -r /opt/ai-search/app/*
 sudo cp target/quarkus-app/app/* /opt/ai-search/app
@@ -8,6 +8,7 @@ sudo rm -r /opt/ai-search/quarkus/*
 sudo cp -r target/quarkus-app/quarkus/* /opt/ai-search/quarkus
 sudo rm -r /opt/ai-search/lib/*
 sudo cp -r target/quarkus-app/lib/* /opt/ai-search/lib
+sudo cp sha256checksums /opt/ai-search/
 sudo bash -c 'echo "$(date --iso-8601=s) $(git rev-parse --verify HEAD)" >> /opt/ai-search/update-linux-deployments.log'
 sleep 5
 sudo systemctl start aisearch
